0fa811e9823c08e276ba7ba89aab9902d281fd0e
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2013-07-03 16:38:41 -0400

    Adds placeholder template

---
+++ README.rst
@@ -0,0 +1,11 @@
+===========
+RVA Connect
+===========
+
+RVA Connect is a web site to share events and spaces in the greater Richmond
+area with a focus on tech
+
+Installation
+============
+
+
--- rvaconnect/urls.py
+++ rvaconnect/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.views.generic.base import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', view=TemplateView.as_view(template_name="holder.html"), name='holder'),
 )
