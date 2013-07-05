e96a4209c2384b5d86e93db7965f631fc481ac8f
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2013-07-05 10:47:42 -0400

    Adios contrib.gis... for now

--- rvaconnect/settings.py
+++ rvaconnect/settings.py
@@ -124,7 +124,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.gis',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
